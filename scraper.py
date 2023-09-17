ef3f7b5269539c9f2292bf7242849bd9c1c42f84
lyfjourney <joshua.farara@gmail.com>
2023-09-16 21:30:28 -0400

    Changes to be committed: 	new file:   chromedriver-win64.zip 	new file:   chromedriver-win64/chromedriver-win64/LICENSE.chromedriver 	modified:   chromedriver.exe 	deleted:    chromedriver_win32.zip 	modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
+import requests
 
-PATH = "C:\Users\User\Desktop\WebScraper\chromedriver.exe"
+# Set the path to chromedriver.exe
+PATH = "C:\\Users\\User\\Desktop\\WebScraper\\chromedriver.exe"
 
-wd = webdriver.Chrome(PATH)
+# Create ChromeOptions and set any desired options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--start-maximized")  # Optional: run Chrome in headless mode
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the webdriver with the options
+wd = webdriver.Chrome(options=chrome_options)
+
+image_url = "https://4kwallpapers.com/images/wallpapers/windows-11-dark-mode-abstract-background-black-background-3840x2160-8710.jpg"
+
+def download_image(dowload_path, url, file_name):
+    image_content = requests.get(url).content
